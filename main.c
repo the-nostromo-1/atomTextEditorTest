6cee59793dfb95eabdabd7280359cdda9221ef0e
David J Tinley <tinman11537@gmail.com>
2022-04-30 21:33:25 -0400

    terminal wont stay open to display the last print message

--- main.c
+++ main.c
@@ -2,6 +2,12 @@
 
 int main()
 {
+  int x, y;
   printf("Hello World!\n");
+  printf("Please enter two numbers:\n");
+  scanf("%d", &x);
+  scanf("%d", &y);
+  printf("You entered %d and %d.\n", x, y);
   return 0;
+  // Atom is being a pain in the butt
 }
